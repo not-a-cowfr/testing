9be6eaaf04dde3a795cf1c6fa28eee7546190594
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-08-27 16:30:19 +0000

    fix codespaces stuff

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,13 +1,17 @@
-apt-get update
-apt-get install -y \
+curl -fsSL https://apt.fury.io/nushell/gpg.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/fury-nushell.gpg
+echo "deb https://apt.fury.io/nushell/ /" | sudo tee /etc/apt/sources.list.d/fury.list
+sudo apt update
+sudo apt install -y \
   nushell \
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+bash
 rustup install nightly
 rustup component add rustfmt
 rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
 rustup component add clippy --toolchain nightly
 
+touch ~/.config/nushell/env.nu
 echo "\$env.config.buffer_editor = \"code\"" > ~/.config/nushell/env.nu
 echo "\$env.config.show_banner = false" >> ~/.config/nushell/env.nu
